26240623647d8ca010fd44306f011b5e53c20e58
jcfpascua <jcfpascua@gmail.com>
2024-09-30 23:49:51 +0800

    Dino Gang 0.1
    - added left and right movement
    - added character idle, walk, and run animations (using walk animation sprites)
    - camera follows character
    - character flips when changing directions

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,145 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed = 5f;
+    public float runSpeed = 8f;
+    Vector2 moveInput;
+
+    public float CurrentMoveSpeed 
+    {
+        get
+        {
+            if (isMoving)
+            {
+                if (isRunning)
+                {
+                    return runSpeed;
+                }
+                else
+                {
+                    return walkSpeed;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+
+    [SerializeField]
+    private bool _isMoving = false;
+    public bool isMoving
+    {
+        get
+        {
+            return _isMoving;
+        }
+        private set
+        {
+            _isMoving = value;
+            animator.SetBool("isMoving", value);
+        }
+    }
+
+
+    [SerializeField]
+    private bool _isRunning = false;
+    public bool isRunning
+    {
+        get
+        {
+            return _isRunning;
+        }
+        private set
+        {
+            _isRunning = value;
+            animator.SetBool("isRunning", value);
+        }
+    }
+
+    public bool _isFacingRight = true;
+    public bool IsFacingRight 
+    {
+        get 
+        {
+            return _isFacingRight;
+        }
+        private set 
+        {
+            if (_isFacingRight != value)
+            {
+                // flip
+                transform.localScale *= new Vector2(-1, 1);
+            }
+            _isFacingRight = value;
+        } 
+    }
+
+    Rigidbody2D rb;
+    Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.velocity.y);
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
+        isMoving = moveInput != Vector2.zero;
+
+        SetFacingDirection(moveInput);
+    }
+
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        // right
+        if (moveInput.x > 0 && !IsFacingRight)
+        {
+            IsFacingRight = true;
+        }
+        // left
+        else if (moveInput.x < 0 && IsFacingRight)
+        {
+            IsFacingRight = false;
+        }
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            isRunning = true;
+        }
+        else if (context.canceled)
+        {
+            isRunning = false;
+        }
+    }
+}
