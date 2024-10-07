a2fe821205fcc1635d6faf4bbe72f09cd517d097
FernyPack <ricovern2001@gmail.com>
2024-10-08 05:27:23 +0800

    Delete Assets/Scripts/Scripts directory

--- Assets/Scripts/Scripts/AnimationStrings.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-internal class AnimationStrings
-{
-    internal static string isMoving = "isMoving";
-    internal static string isRunning = "isRunning";
-}
