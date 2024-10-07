a59c8e241f2b012632ad88f727f868f471203f6e
FernyPack <ricovern2001@gmail.com>
2024-10-08 05:29:06 +0800

    Dialogs for Win/Lose and ReturnToMainMenu Button

--- Assets/Scripts/TriggerCollision.cs
+++ Assets/Scripts/TriggerCollision.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TriggerCollision : MonoBehaviour
 {
-    // public GameObject self;
-    public void OnTriggerEnter2D(Collider2D other) {
-            if (other.gameObject.tag == "Pitfall") {
-                Debug.Log("I'M BACK IN THE FUCKING BUILDING AGAIN");
-            } 
-        // else if (other.gameObject.CompareTag("Finish")) {
-        //     Debug.Log("martha i'm coming home sweetie");
-        // }
-        // Debug.Log("FUUUUUUUUUUUCK");
+    public float fallThreshold = -8f; // Y position threshold for losing
+
+    void Update()
+    {
+        // Check if player falls below fallThreshold
+        if (transform.position.y <= fallThreshold)
+        {
+            SceneManager.LoadScene("Lose Scene");  // Load the lose scene
+        }
+    }
+
+    // Trigger when player finishes the level (e.g., collides with end point)
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Finish"))  // Make sure the finish line has the tag "Finish"
+        {
+            SceneManager.LoadScene("Win Scene");  // Load the win scene
+        }
     }
 }
